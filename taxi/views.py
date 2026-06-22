46630e96a59d3aa633a3a118a4bdf290eb859242
Denys Matusevych <denytop3@gmal.com>
2026-06-22 22:52:38 +0300

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView, DetailView
+
 from taxi.models import Driver, Car, Manufacturer
 
 
@@ -13,3 +15,31 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").order_by("id")
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").prefetch_related("drivers")
+
+
+class DriverListView(ListView):
+    model = Driver
+    queryset = Driver.objects.all()
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
